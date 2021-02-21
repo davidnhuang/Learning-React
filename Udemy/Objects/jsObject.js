18b86dccff8899e488127d8ccf40acf75d32ba28
David <David.N.Huang@gmail.com>
2021-02-20 19:46:30 -0800

    Props for functions and classes

---
+++ Udemy/Objects/jsObject.js
@@ -0,0 +1,27 @@
+// Challenge
+// build a person with object literal notation
+
+var humanBeing= {
+    hungry: true,
+    age: 25,
+    height: "5ft 10in"
+};
+
+// no build a Pizza
+var pizza= {};
+
+pizza.cheese = "mozzarella";
+pizza.slices = 6;
+pizza.topping = "pepperoni";
+pizza.size = 12;
+
+var animal = {};
+
+animal[canMove] = true;
+animal.appendages = 4;
+animal.name = "cat";
+
+var cup = {};
+cup["insulated"] = true;
+cup["handles"] = 1;
+
